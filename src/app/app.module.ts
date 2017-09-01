01be024c31c6da275fc07196c1b9cfcc56eb4a0d
alexFrieden <alex@frieden.org>
2017-09-01 15:42:07 -0400

    code where Table does not work

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective
+  ],
+  imports: [
+    BrowserModule,
+    CdkTableModule
+  ],
+  exports: [CdkTableModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
